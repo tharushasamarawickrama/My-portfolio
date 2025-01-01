bd47cad5d791e09ee945977409ff5e55b276be9b
tharushasamarawickrama <140165950+tharushasamarawickrama@users.noreply.github.com>
2025-01-01 20:52:08 +0530

    complete navbar and start hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+
+
+const Hero = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+        <div className="flex flex-wrap">
+            <div className="w-full lg:w-1/2 ">
+                <div className="flex flex-col item-center lg:items-start">
+                    <h1 className="pb-16 text-5xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Tharusha samarawickrama</h1>
+                    <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+                    Full Stack Developer
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Hero
