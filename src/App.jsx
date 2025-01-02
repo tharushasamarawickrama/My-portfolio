46ad117618b11bcfe2b632e3ac0d27fa0886b599
tharushasamarawickrama <140165950+tharushasamarawickrama@users.noreply.github.com>
2025-01-02 12:09:40 +0530

    complete experience section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Experience from "./components/Experience";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Technologies from "./components/Technologies";
@@ -15,7 +16,8 @@ const App = () => {
         <Navbar />
         <Hero />
         <About />
-        <Technologies />
+        <Technologies /> 
+        <Experience />
       </div>
       
     </div>
