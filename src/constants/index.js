8c04ba6c9b93a4b81fc6765cddcb6b9423b80ac8
tharushasamarawickrama <140165950+tharushasamarawickrama@users.noreply.github.com>
2025-01-02 20:40:28 +0530

    edit details

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "No:32,Wajiragnana Mawatha,Baddegama,Walgama,Matara ",
+  phoneNo: "+9470 430 8123 ",
+  email: "tharushathathsaraviduranga@gmail.com",
 };
