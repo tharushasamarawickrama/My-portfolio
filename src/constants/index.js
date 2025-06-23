ca867e40ea829432b0b5c56a8e87197c855ec846
tharushasamarawickrama <140165950+tharushasamarawickrama@users.noreply.github.com>
2025-06-23 22:30:01 +0530

    do change

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,7 +42,7 @@ export const EXPERIENCES = [
 export const PROJECTS = [
   {
     title: "E-Commerce Website",
-    image: BloodLife,
+    image: project1,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
